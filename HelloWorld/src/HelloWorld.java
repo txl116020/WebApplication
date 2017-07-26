e3cc1fc0289d1271dbb092fca9b4d98b9884b5ff
tluong <txl116020@gmail.com>
2017-07-25 19:58:08 -0500

    1st program

---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author tluon
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("What is up????");
+
+	}
+
+}
